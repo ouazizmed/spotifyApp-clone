8f8f2bf812ca95c42cf67448a04cbc723e15d8a0
ouaziz <ouazizmed2@gmail.com>
2020-09-19 01:37:43 +0100

    get Token from url

--- src/Login.js
+++ src/Login.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './Login.css';
-
+import { loginUrl } from "./spotify";
+ 
 function Login(){
     return(
         <div className="login">
             <h1>I am the login page</h1>
             <img src= "https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt=""/>
-            <a>LOGIN WITH SPOTIFY</a>
+            <a href={loginUrl}>LOGIN WITH SPOTIFY</a>
         </div>
     )
 }
 
-export default Login
+export default Login;
